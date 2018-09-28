bb5935de2963b03b6ddddc5cc1a16c19aac8efbe
家通的MBP <apple@RagtagGeoduck.local>
2018-09-28 08:49:42 +0800

    add files

---
+++ mainbody.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: apple
+ * Date: 2018/9/21
+ * Time: 9:10 AM
+ */?>
+
+<html>
+<head>
+    <title>学生成绩管理系统</title>
+</head>
+<body margintop="0" marginleft="0" marginbottom="0" marginright="0">
+<table width="850" border="0" align="center" cellpadding="0" cellspacing="0">
+    <tr>
+        <td><img src="images/学生成绩管理系统.gif" width="850" height="120"></td>
+    </tr>
+    <tr>
+        <td><iframe src="stu_frame.html" width="850" height="500"></iframe></td>
+    </tr>
+    <tr>
+        <td><img src="images/底端图片.gif" width="850" height="45"></td>
+    </tr>
+</table>
+</body>
+</html>
